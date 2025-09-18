8076fc903e3ad30137c9485a3936e65d596ba9c5
VISVANATH R <158147312+VISVANATH123@users.noreply.github.com>
2025-09-18 13:59:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function indexOfIgnoreCase(s1, s2):number {
-  // write your code here
-  const v1=s1.toLowerCase();
-  const v2=s2.toLowerCase();
+function indexOfIgnoreCase(s1: string, s2: string): number {
+  if (!s1 || !s2) return -1; // handle null or empty inputs
+
+  const v1 = s1.toLowerCase();
+  const v2 = s2.toLowerCase();
+
   return v1.indexOf(v2);
 }
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+alert(indexOfIgnoreCase(s1 ?? "", s2 ?? ""));
